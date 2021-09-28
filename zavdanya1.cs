50e67ee365bb5df9d18147307f4bed65e01df3d7
DmitriyZhebratkin <84238002+DmitriyZhebratkin@users.noreply.github.com>
2021-09-28 21:19:45 +0300

    first commit

---
+++ zavdanya1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Laboratorka2
+{
+    class Address
+
+    {
+
+        public string index; // индекс
+
+        public string country;     // Страна
+
+        public string city;     // город
+
+        public string street;     // улица
+
+        public int house;     //номер улицы
+
+        public int apartment;     // квартира
+
+        public void GetInfo()
+
+        {
+
+            Console.WriteLine($"index: {index} \ncountry: {country} \ncity: {city} \nstreet: {street} \nhouse: {house} \napartment: {apartment}");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Address home = new Address();
+
+            home.index = "06750";
+
+            home.country = "Ukraine";
+
+            home.city = "Kyiv";
+
+            home.street = "Shevchenka";
+
+            home.house = 69;
+
+            home.apartment = 12;
+
+            home.GetInfo();  
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
